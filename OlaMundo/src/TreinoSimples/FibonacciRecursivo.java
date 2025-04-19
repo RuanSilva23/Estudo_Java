349c1496dc35321fd36973955559f8eb64e9134c
Ruan Silva <ruan.silva2002@gmail.com>
2025-04-18 22:19:17 -0300

    Update FibonacciRecursivo.java

--- OlaMundo/src/TreinoSimples/FibonacciRecursivo.java
+++ OlaMundo/src/TreinoSimples/FibonacciRecursivo.java
@@ -1,4 +1,29 @@
 package TreinoSimples;
 
+import java.util.Scanner;
+
 public class FibonacciRecursivo {
+    public static int fibonacci(int n){
+        if (n==0){
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos termos: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i <=n; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        scanner.close();
+
+    }
 }
